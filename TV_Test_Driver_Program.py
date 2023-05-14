6e7e28c3608fbd46e72f682227374ce91d3de2eb
unknown <arixxdelera21@gmail.com>
2023-05-14 11:45:07 +0800

    pseudocode has been added

---
+++ TV_Test_Driver_Program.py
@@ -0,0 +1,27 @@
+# Delera, Aritz B.
+
+# Exercise:
+# Given: A UML Class Diagram below:
+# Required: Create a Python Code for creating the Class named TV
+# and a Test Driver program named TestTV that will create two objects from Class TV 
+# and will produce the following output:
+
+# PSEUDOCODE
+# Create Class 
+# Use Class Variable to initialize the values of channel and volume
+# Create Constructor
+# Create Instances
+# Turn on the TV
+# Turn off the TV
+# Get the Channel
+# Set the Channel
+# Get the Volume
+# Set the Volume
+# Use channelUp to increment the channel by 1 if it is in a minimum value
+# Use channelDown to decrement the channel by 1 if it is in a maximum value
+# Use volumeUp to increment the volume by 1 if it is in a minimum value
+# Use volumeDown to decrement the volume by 1 if it is in a maximum value
+
+# Create test driver program class named TestTV
+# Create two instances; tv1 and tv2
+# Print the intances
